8493ad6b46b88cf80d19a6e297b0162095989a43
stanken_a <artyemst3@mail.ru>
2023-12-16 22:49:27 +0300

    feat: add DateInfo

--- src/components/DateInfo/DateInfo.tsx
+++ src/components/DateInfo/DateInfo.tsx
@@ -1,9 +1,68 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./DateInfo.module.scss";
 import CommonCard from "../CommonCard/CommonCard";
+import { Icons } from "@/ui-kit/Icons";
 
 const DateInfo: FC = () => {
-  return <CommonCard title='Date info'>bruh</CommonCard>;
+  const date = new Date();
+  const today = date.toDateString().split(" ");
+
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  const [time, setTime] = useState({
+    hour: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+  });
+
+  useEffect(() => {
+    if (!isHydrated) return;
+
+    function setFreshTime() {
+      const newDate = new Date();
+      setTime({
+        hour: newDate.getHours(),
+        minutes: newDate.getMinutes(),
+        seconds: newDate.getSeconds(),
+      });
+    }
+
+    const interval = setInterval(() => setFreshTime(), 1000);
+
+    return () => clearInterval(interval);
+  }, [isHydrated]);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  return (
+    <CommonCard title='Date info'>
+      <div className={styles.content}>
+        <div className={styles.block}>
+          <p className={styles.block__title}>Current time is</p>
+          <div className={styles.block__container}>
+            <Icons.Clock />
+            <p className={styles.block__value}>
+              {isHydrated &&
+                `${time.hour > 10 ? time.hour : "0" + time.hour}:${
+                  time.minutes > 10 ? time.minutes : "0" + time.minutes
+                }:${time.seconds > 10 ? time.seconds : "0" + time.seconds}`}
+            </p>
+          </div>
+        </div>
+        <div className={styles.block}>
+          <p className={styles.block__title}>Today is</p>
+          <div className={styles.block__container}>
+            <Icons.Calendar />
+            <p
+              className={styles.block__value}
+            >{`${today[2]} ${today[1]} ${today[3]}`}</p>
+          </div>
+        </div>
+      </div>
+    </CommonCard>
+  );
 };
 
 export default DateInfo;
--- src/ui-kit/Icons/Icons.tsx
+++ src/ui-kit/Icons/Icons.tsx
@@ -440,6 +440,7 @@ const Profile: FC = () => (
     />
   </svg>
 );
+
 const Dots: FC = () => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
@@ -453,6 +454,70 @@ const Dots: FC = () => (
       <rect x='11' y='0.5' width='6' height='6' rx='3' fill='#F9F9F9' />
       <rect x='22' y='0.5' width='6' height='6' rx='3' fill='#F9F9F9' />
     </g>
+  </svg>
+);
+
+const Clock: FC = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+  >
+    <path
+      d='M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z'
+      stroke='#F9F9F9'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M12 6V12L16 14'
+      stroke='#F9F9F9'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+const Calendar: FC = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+  >
+    <path
+      d='M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z'
+      stroke='#F9F9F9'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M16 2V6'
+      stroke='#F9F9F9'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M8 2V6'
+      stroke='#F9F9F9'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M3 10H21'
+      stroke='#F9F9F9'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
   </svg>
 );
 
@@ -472,4 +537,6 @@ export {
   Sun,
   Profile,
   Dots,
+  Clock,
+  Calendar,
 };
